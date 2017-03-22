924f0dc09ec09423c15e0c3383e14741f869530b
Adam <a.lister1@lancaster.ac.uk>
2017-03-22 15:43:22 -0500

    fixing indentation

--- easy_plot.cxx
+++ easy_plot.cxx
@@ -19,100 +19,100 @@
 std::vector < std::vector < double> > load_file(const char * file_name)
 {
 
-	std::vector < std::vector < double > > file_vector;
-	std::ifstream myfile;
-	myfile.open(file_name);
+    std::vector < std::vector < double > > file_vector;
+    std::ifstream myfile;
+    myfile.open(file_name);
 
-	//some exception handling for file io
-	if(!myfile.is_open())
-	{
-		throw "File is not open!";
-	}
-	else
-	{
-		std::cout << "File opened: " << file_name << std::endl;
-	}
+    //some exception handling for file io
+    if(!myfile.is_open())
+    {
+        throw "File is not open!";
+    }
+    else
+    {
+        std::cout << "File opened: " << file_name << std::endl;
+    }
 
-	int events;
-	int x;
-	int y;
-	int counter = 0;
+    int events;
+    int x;
+    int y;
+    int counter = 0;
 
-	std::string line;
-	std::vector<double> data_vector;
+    std::string line;
+    std::vector<double> data_vector;
 
-	//read the data out from the file
-	//this readout assumes that values are ',' separated
-	//also attempts to handle values which cannot be converted
-	//from string to double
-	while( !myfile.eof() )
-	{
-		std::string s;
-		if (!getline( myfile, s )) break;
-		std::istringstream ss( s );
-		while (ss)
-		{
-			std::string s;
-			if (!getline( ss, s, ',' )) break;
-			double data;
-			try
-			{
-				data = std::stod(s);
-			}
-			catch(const std::exception& ex)
-			{
-				std::cerr << "No convertion to double from string... skipping" << std::endl;
-				break;
-			}
+    //read the data out from the file
+    //this readout assumes that values are ',' separated
+    //also attempts to handle values which cannot be converted
+    //from string to double
+    while( !myfile.eof() )
+    {
+        std::string s;
+        if (!getline( myfile, s )) break;
+        std::istringstream ss( s );
+        while (ss)
+        {
+            std::string s;
+            if (!getline( ss, s, ',' )) break;
+            double data;
+            try
+            {
+                data = std::stod(s);
+            }
+            catch(const std::exception& ex)
+            {
+                std::cerr << "No convertion to double from string... skipping" << std::endl;
+                break;
+            }
 
-			//assumes that we have 3 values per line
-			//file assumes 3 data products per line
-			if(counter == 3)
-			{
-				file_vector.push_back(data_vector);
-				data_vector.clear();
-				counter = 0;
-			}
-			if(counter != 3)
-			{
-				data_vector.push_back(data);
-				counter++;
-			}
-		}
-	}
-	if(!data_vector.empty()) {file_vector.push_back(data_vector); data_vector.clear(); }
+            //assumes that we have 3 values per line
+            //file assumes 3 data products per line
+            if(counter == 3)
+            {
+                file_vector.push_back(data_vector);
+                data_vector.clear();
+                counter = 0;
+            }
+            if(counter != 3)
+            {
+                data_vector.push_back(data);
+                counter++;
+            }
+        }
+    }
+    if(!data_vector.empty()) {file_vector.push_back(data_vector); data_vector.clear(); }
 
 
-	myfile.close();
+    myfile.close();
 
-	return file_vector;
+    return file_vector;
 
 }
 
 //histogram plotting function
 void plot_histogram(const char * h_name,
-                    const char * h_title,
-                    const char * file_name,
-                    const char * axis_title_x,
-                    const char * axis_title_y,
-                    std::vector < std::vector < double> > vector_name
-                    )
+        const char * h_title,
+        const char * file_name,
+        const char * axis_title_x,
+        const char * axis_title_y,
+        std::vector < std::vector < double> > vector_name
+        )
 {
-	std::sort(vector_name.begin(), vector_name.end());
+    std::sort(vector_name.begin(), vector_name.end());
 
-    const double max_bin = 20; //vector_name.back().at(1);//grab max x
+    const double max_bin = vector_name.back().at(1);//grab max x
     const double min_bin = 0; //vector_name.front().at(1);//grab min x
 
-	TCanvas * c1 = new TCanvas();
-	c1->cd();
+    TCanvas * c1 = new TCanvas();
+    c1->cd();
     c1->SetGrid();
-	TH1D * h1 = new TH1D(h_name, h_title, 104, min_bin, max_bin);
-	TH1D * h2 = new TH1D(h_name, h_title, 104, min_bin, max_bin);
+    TH1D * h1 = new TH1D(h_name, h_title, 104, min_bin, max_bin);
+    TH1D * h2 = new TH1D(h_name, h_title, 104, min_bin, max_bin);
 
-	for(int i = 0; i < vector_name.size(); i++)
-	{
-		h1->Fill(vector_name.at(i).at(1), vector_name.at(i).at(2));
-	}
+    for(int i = 0; i < vector_name.size(); i++)
+    {
+        h1->Fill(vector_name.at(i).at(1), vector_name.at(i).at(2));
+    }
 
     // plot cumulative sum
     double cumulativeSum = 0;
@@ -127,10 +127,10 @@ void plot_histogram(const char * h_name,
     leg->AddEntry(h2, "CumulativeSum");
 
     h1->SetMaximum(3.0);
-	h1->GetXaxis()->SetTitle(axis_title_x);
-	h1->GetYaxis()->SetTitle(axis_title_y);
-	h2->SetLineColor(kGreen-8);
-    
+    h1->GetXaxis()->SetTitle(axis_title_x);
+    h1->GetYaxis()->SetTitle(axis_title_y);
+    h2->SetLineColor(kGreen-8);
+
     h1->Draw("hist");
     h2->Draw("same");
     leg->Draw("same");
@@ -144,24 +144,24 @@ void plot_histogram(const char * h_name,
 //try running functions here
 int main()
 {
-	//file name
-	const char * in_file = "plot_data.txt";
+    //file name
+    const char * in_file = "plot_data.txt";
 
-	std::vector < std::vector < double > > plot_data;
+    std::vector < std::vector < double > > plot_data;
 
-	//if problem opening and loading file, exit
-	try
-	{
-		plot_data = load_file(in_file);
-	}
-	catch(const char * msg)
-	{
-		std::cerr << msg << std::endl;
-		exit(1);
-	}
+    //if problem opening and loading file, exit
+    try
+    {
+        plot_data = load_file(in_file);
+    }
+    catch(const char * msg)
+    {
+        std::cerr << msg << std::endl;
+        exit(1);
+    }
 
-	//see function for variables
-	plot_histogram("h_test", "h_title", "h_title.pdf", "x", "y", plot_data);
+    //see function for variables
+    plot_histogram("h_test", "h_title", "h_title.pdf", "x", "y", plot_data);
 
-	return 0;
+    return 0;
 }
